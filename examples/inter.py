e47fa0bc61fe51a633f4c9cbf6c7fd3f6be36899
Brandon Bowles (PI2) <BrandonBowles@gmail.com>
2015-06-08 20:27:17 +0000

    Misc changes/fixes
    Switch Player 1/2
    Make MCPs auto-adjust
    Many many more

--- examples/inter.py
+++ examples/inter.py
@@ -8,22 +8,23 @@ import sys
 import math
 import traceback
 import time
+import subprocess
 
 keymap = [
-	e.KEY_A, e.KEY_W, e.KEY_D, e.KEY_S, e.KEY_F, e.KEY_R, e.KEY_G, e.KEY_T, e.KEY_H, e.KEY_Y,
-	e.KEY_LEFT, e.KEY_UP, e.KEY_RIGHT, e.KEY_DOWN, e.KEY_ENTER, e.KEY_SPACE, e.KEY_HOME, e.KEY_END, e.KEY_PAGEUP, e.KEY_PAGEDOWN,
-	e.KEY_J, e.KEY_I, e.KEY_L, e.KEY_K, e.KEY_7, e.KEY_U, e.KEY_8, e.KEY_9,
-	e.KEY_Z, e.KEY_X, e.KEY_C, e.KEY_B, e.KEY_N, e.KEY_M, e.KEY_Q, e.KEY_E
+	e.KEY_1, e.KEY_LEFT, e.KEY_UP, e.KEY_RIGHT, e.KEY_DOWN, e.KEY_ENTER, e.KEY_SPACE, e.KEY_HOME, e.KEY_END, e.KEY_PAGEUP, e.KEY_PAGEDOWN,
+	e.KEY_2, e.KEY_A, e.KEY_W, e.KEY_D, e.KEY_S, e.KEY_F, e.KEY_R, e.KEY_G, e.KEY_T, e.KEY_H, e.KEY_Y,
+	e.KEY_3, e.KEY_J, e.KEY_I, e.KEY_L, e.KEY_K, e.KEY_7, e.KEY_U, e.KEY_8, e.KEY_9,
+	e.KEY_4, e.KEY_Z, e.KEY_X, e.KEY_C, e.KEY_B, e.KEY_N, e.KEY_M, e.KEY_Q, e.KEY_E
 	]
-btnmap = [
-	0,1,2,3,4,5,6,7,8,9,
-	10,11,12,13,14,15,16,17,18,19,
-	20,21,22,23,24,25,26,27,
-	28,29,30,31,32,33,34,35
-	]
-gpios = [15,18,23,24]
-mcps = [[0x25,4,0,0],[0x26,14,16,0]]
-labels = ["left","up","right","down","topleft","botleft","topmid","botmid","toprt","botrt"]
+btnmap = []
+goodgpios=[4,7,8,9,10,11,14,15,17,18,22,23,24,25,27,28]
+gpios = []
+#mcps = [[0x25,4,0,0,[]],[0x26,27,16,0,[]]]
+mcps = []
+labels = ["start","left","up","right","down","topleft","botleft","topmid","botmid","toprt","botrt"]
+gpiostate = {}
+gpiopins = []
+callbacks = []
 
 def readconfig():
 	try:
@@ -31,104 +32,266 @@ def readconfig():
 	except IOError as ie:
 		fh = open("inter.cfg","w+")
 	data = fh.readlines()
+	fh.close()
 	if (len(data)==0):
 		fh = open("inter.cfg","w+")
-		fh.write(",".join(str(x) for x in btnmap))
+		fh.write(",".join(str(x) for x in range(0,40)))
 		fh.close()
 	if (len(data)>0):
-		l1 = data[0].split(",")
-		if (len(l1)==36):
+		l1 = data[0].rstrip().split(",")
+		if (len(l1)==40):
 			bmi=-1
 			for x in l1:
 				bmi = bmi + 1
-				btnmap[bmi] = int(x)
+				btnmap.append(int(x))
 			print("Button Config Loaded: %s" % (",".join(str(x) for x in btnmap)))
 	if (len(data)>1):
-		l2 = data[1].split(",")
-		gpios=",".join(int(x) for x in data[1])
+		l2 = data[1].rstrip().split(",")
+		for x in l2:
+			if(x!=""):
+				gpios.append(int(x))
 	if (len(data)>2):
-		l3 = data[2].split(",")
-		mcpcnt = len(l3)/2
-		mcps = []
-		for i in range(0,mcpcnt+1):
-			mcps.append([int(l3[i*2],16),l3[(i*2)+1],16*i,0])
+		l3 = data[2].rstrip().split(",")
+		mcpi = 0
+		for i in range(0,len(l3)):
+			if (l3[i] != ""):
+				mcp = MCP23017(address = int(l3[mcpi]), num_gpios = 16) # MCP23017
+				mcps.append([l3[mcpi],0,(16*i),mcp,[]])
+				mcpi = mcpi + 1
+	if (len(data)>3):
+		l4 = data[3].rstrip().split(",")
+		if(len(l4)==len(mcps)):
+			mcpi = 0
+			for x in l4:
+				mcps[mcpi][1] = int(x)
+				mcpi = mcpi + 1
+	mcptxt=", ".join("%s %s" % (x[0],x[1]) for x in mcps)
+	gpiotxt=",".join(str(x) for x in gpios)
+	print("Current Config: MCPs: %s GPIOs: %s" % (mcptxt,gpiotxt))
+
+def i2cd():
+	raw = subprocess.check_output(["i2cdetect -y 1 | sed -e 's:^....::' | tail -n +3 | grep [0-9] | sed -e 's: \-\-::g' | tr ' ' '\\n' | grep [0-9] | tr '\\n' ' ' | sed -e 's:[^0-9]: :g' | sed -e 's:\s*$::'"], shell=True).decode("UTF8").split(" ")
+	i=0
+	ret=[]
+	for x in raw:
+		if(x!=""):
+			ret.append(int(x,16))
+	return ret
 
 def setup():
+	taken = []
+	i2cs = i2cd()
+	callbacks = []
+	print("Found I2C devices: %s" % (", ".join(str(x) for x in i2cs)))
+	for mcpi in range(0,len(mcps)):
+		if(type(mcps[mcpi][3]).__name__=="MCP23017"):
+			mcps[mcpi][3].cleanup()
+	for pin in goodgpios:
+		GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	for addy in i2cs:
+		while True:
+			gpio=input("Enter callback pin for MCP at %s: " % (addy))
+			if(gpio == ""):
+				break
+			if(gpio not in taken):
+				taken.append(gpio)
+				break
+			else:
+				print("Button %s already used" % gpio)
+				time.sleep(0.5)
+		if(gpio!=""):
+			callbacks.append(gpio)
+			time.sleep(0.5)
+		else:
+			break
+	
+	GPIO.cleanup()
+	for x in gpiostate:
+		gpiostate.remove(x)
+	for mcpi in range(0,len(mcps)):
+		print("Setting up %s on %s" % (mcps[mcpi][0],callbacks[mcpi]))
+		mcp = setupMCP(int(mcps[mcpi][0]),int(callbacks[mcpi]))
+		mcps[mcpi][3] = mcp[0]
+		mcps[mcpi][4] = mcp[1]
+	for pin in goodgpios:
+		if(str(pin) not in callbacks):
+			GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+		
 	bmi = -1
 	for player in range(1,5):
 		ps = "Player %d" % (player)
 		lbli = 0
 		for label in labels:
 			lbli = lbli + 1
-			if (player > 2 and lbli > 8):
+			if (player > 2 and lbli > 9):
 				continue
 			bmi = bmi + 1
-			cur = btnmap[bmi]
-			print("Press button for %s %s [%s]: " % (ps, label, cur))
-			btn=pollall()
-			btn=pollall()
+			if(bmi in btnmap):
+				cur = btnmap[bmi]
+			else:
+				cur = 0
+			while True:
+				btn=pollall("Press button for %s %s [%s]: " % (ps, label, cur))
+				if(btn not in taken):
+					taken.append(btn)
+					break
+				else:
+					print("Button %s already used" % btn)
+					time.sleep(0.5)
 			print("Button pressed: %s" % (btn))
 			btnmap[bmi] = btn
 			time.sleep(0.5)
+
+	print(",".join(str(x) for x in btnmap))
+	print(",".join(str(x) for x in gpios))
+	print(",".join(str(x) for x in i2cs))
+	print(",".join(callbacks))
+
 	fh = open("inter.cfg","w+")
-	fh.write(",".join(str(x) for x in btnmap))
+
+	for i in range(0,len(btnmap)):
+		fh.write(str(btnmap[i]))
+		if(i < len(btnmap) - 1):
+			fh.write(",")
+		else:
+			fh.write("\n")
+	fh.write("%s\n" % ",".join(str(x) for x in gpios))
+	fh.write("%s\n" % ",".join(str(x) for x in i2cs))
+	fh.write("%s\n" % ",".join(callbacks))
+	
 	fh.close()
 
-def pollall():
-	mcpi = -1
+def getgpio(pin):
+	if (str(pin) in callbacks):
+		return False
+	if (pin < 32 and pin in goodgpios):
+		if (pin not in gpiostate):
+			GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		try:
+			state = GPIO.input(pin)
+		except Exception as e:
+			state = gpiostate[pin]
+	elif (pin >= 32):
+		mcpi = int(pin / 32) - 1
+		pin = pin % 16
+		if(mcpi in mcps and type(mcps[mcpi][3]).__name__=="MCP23017"):
+			state = mcps[mcpi][3].input(pin)
+		else:
+			return -1
+	else:
+		return -1
+	gpiostate[pin] = state
+	return state
+
+def pollall(prompt=False, mcp=True):
+	starts = {}
+	# 1st set defaults
+	for pin in goodgpios:
+		starts[pin] = getgpio(pin)
+	if(mcp == True):
+		for mcpi in range(0,len(mcps)):
+			mcp = mcps[mcpi][3]
+			if(type(mcp).__name__!="MCP23017"):
+				continue
+			for ch in range(0,16):
+				pin=32+(16*mcpi)+ch
+				val=mcp.input(ch)
+				starts[pin]=val
+
+	# 2nd show prompt
+	if(prompt != False):
+		print(prompt)
+		time.sleep(0.5)
+
+	# Now poll
 	while True:
-		for mcpa in mcps:
-			mcpi = mcpi + 1
-			for pin in range(0,16):
-				val=mcpa[3].input(pin)
-				if (val==0):
-					return int(mcpi*16) + int(pin)
-		gpi=-1
-		for gpio in gpios:
-			gpi = gpi + 1
-			val=GPIO.input(gpio)
-			if (val==0):
-				return int(16*len(mcps))+int(gpi);
+		for pin in goodgpios:
+			val=getgpio(pin)
+			if(starts[pin]!=val):
+				if(prompt != False):
+					print(pin)
+				return pin
+		if(mcp == False):
+			continue
+		for mcpi in range(0,len(mcps)):
+			mcp = mcps[mcpi][3]
+			for ch in range(0,16):
+				pin=32+(16*mcpi)+ch
+				val=mcp.input(ch)
+				if(pin not in starts):
+					starts[pin] = val
+				if(starts[pin]!=val):
+					if(prompt != False):
+						print(pin)
+					return pin
 
 # interrupt callback function
 # just get the pin and values and print them
 def intcall(k, val):
 	mcpi=-1
 	for mcpa in mcps:
+		if(type(mcpa[3]).__name__!="MCP23017"):
+			continue
 		mcpi = mcpi + 1
 		pin, value = mcpa[3].readInterrupt()
 		if (type(pin).__name__=="int"):
-			pin = (mcpi*16)+pin;
-			try:
+			pin = 32+(mcpi*16)+pin;
+			gpin=pin
+			if(pin in btnmap):
 				pin=btnmap.index(pin)
-			except Exception as ex:
-				if (verbose):
-					traceback.print_ex()
 			kc=keymap[pin]
 			if (verbose):
-				lbli=bm%10
-				pl=math.floor(bm/10) + 1
-				if(pin>20):
-					lbli=(bm-20)%8
-					pl=math.floor((bm-20)/8) + 1
+				lbli=pin%10
+				pl=math.floor(pin/11) + 1
+				if(pin>32):
+					lbli=(pin-33)%10
+					pl=math.floor((pin-20)/10) + 1
 				lbl=labels[lbli]
-				print("I(%d) %s %s %s %s %s" % (mcpi + 1, pin, value, kc, pl, lbl))
+				if (value==0):
+					print("I(%d) %s %s %s %s %s %s" % (mcpi + 1, gpin, pin, value, kc, pl, lbl))
 			ui.write(e.EV_KEY, kc, 1 - value)
 			ui.syn()
 		
 def btncall(pin, value):
 # 	if (type(pin).__name__=="int"):
 	gpin=pin
-	pin=(16*len(mcps))+gpios.index(pin)
-	bm=btnmap.index(pin)
-	kc=keymap[bm]
-	if (verbose):
+	if(pin in btnmap):
+		pin = btnmap.index(pin)
+# 	if(bm not in keymap):
+# 		print("%s not found" % bm)
+# 		return
+	kc = keymap[pin]
+	if (verbose and value==0):
 		print("G(%d) %s %s %s" % (gpin, pin, value, kc))
 	ui.write(e.EV_KEY, kc, 1 - value)
 	ui.syn()
+	
+def setupMCP(addy,callpin):
+	if(verbose):
+		print("Callback: %s: %s" % (addy, callpin))
+	mcp = MCP23017(address = addy, num_gpios = 16)
+	mcp.configSystemInterrupt(mcp.INTMIRRORON, mcp.INTPOLACTIVEHIGH)
+	states = []
+	for pin in range(0,16):
+		mcp.pinMode(pin, mcp.INPUT)
+		mcp.pullUp(pin, 1)
+		mcp.configPinInterrupt(pin, mcp.INTERRUPTON, mcp.INTERRUPTCOMPAREPREVIOUS)
+		val = mcp.input(pin)
+		states.append(mcp.input(pin))
+	GPIO.add_interrupt_callback(callpin, intcall, edge='both', pull_up_down = GPIO.PUD_DOWN)
+	gpiostate[callpin] = 2
+	return [mcp,states]
+	
+def cleanup():
+	for addy in i2cd():
+		mcp = MCP23017(address = addy, num_gpios = 16)
+		mcp.cleanup()
+	GPIO.cleanup()
 
 try:
 	ui = UInput(name="retrogame")
+	
+	i2cs = i2cd()
 	
 	readconfig()
 
@@ -137,28 +300,23 @@ try:
 	verbose = 0
 	mode = 1
 	if (len(sys.argv) == 2 and sys.argv[1] == "setup"):
-		mode = 2
+		setup()
 	if (len(sys.argv) == 2 and sys.argv[1] == "verbose"):
 		verbose = 1
 	
-	for mcpa in mcps:
-		mcp = MCP23017(address = mcpa[0], num_gpios = 16) # MCP23017
-		if (mode == 1):
-			mcp.configSystemInterrupt(mcp.INTMIRRORON, mcp.INTPOLACTIVEHIGH)
+	for mcpi in range(0,len(i2cs)):
+		addy=i2cs[mcpi]
+		if(mcpi not in mcps):
+			mcps.append([addy,4,32+(mcpi*16),0,[]]);
+		mcpa = mcps[mcpi]
 
-		for pin in range(0,16):
-			mcp.pinMode(pin, mcp.INPUT)
-			mcp.pullUp(pin, 1)
-			if (mode == 1):
-				mcp.configPinInterrupt(pin, mcp.INTERRUPTON, mcp.INTERRUPTCOMPAREPREVIOUS)
-
-		if (mode == 1):
-			GPIO.add_interrupt_callback(mcpa[1], intcall, edge='both', pull_up_down=GPIO.PUD_DOWN)
-		
-		mcps[mcpi][3] = mcp
-		mcpi = mcpi + 1
+		mcpp = setupMCP(i2cs[mcpi], mcpa[1])
+		mcps[mcpi][3] = mcpp[0]
+		mcps[mcpi][4] = mcpp[1]
 	
 	for pin in gpios:
+		if (verbose):
+			print("Setting up %s" % pin)
 		GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		if (verbose):
 			print("%s %s" % (pin, GPIO.input(pin)))
@@ -182,14 +340,11 @@ try:
 			# three times in a row, .5 seconds apart. if it gets to the end of 3 
 			# iterations, we're probably stuck so it will reset
 			for mcpa in mcps:
-				if (type(mcpa[3]).__name__!="int"):
+				if (type(mcpa[3]).__name__=="MCP23017"):
 					mcpa[3].clearInterrupts()
 
 except Exception as e:
 	traceback.print_exc()
 finally:
-	for mcpa in mcps:
-		if(type(mcpa[3]).__name__!="int"):
-			mcpa[3].cleanup()
-	GPIO.cleanup()
+	cleanup()
 	ui.close()
